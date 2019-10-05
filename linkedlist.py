6f519516394ea6aae99034d7199754f89bd0fbb5
Elliot Briant <elliotbriant@Elliots-MacBook-Air.local>
2019-10-05 11:38:13 -0700

    Adding stacks

--- linkedlist.py
+++ linkedlist.py
@@ -1,7 +1,7 @@
-class Element(object): 
+class Element(object):
     def __init__(self, value):
         self.value = value
-        self.next = next
+        self.next = None
 
 class LinkedList(object):
     def __init__(self, head=None):
@@ -9,47 +9,32 @@ class LinkedList(object):
 
     def append(self, new_element):
         current = self.head
-        if self.head: 
+        if self.head:
             while current.next:
                 current = current.next
             current.next = new_element
         else:
             self.head = new_element
-    
-    def get_position(self, position):
-        counter = 1
-        current = self.head
-        if position < 1:
+
+    def insert_first(self, new_element):
+        new_element.next = self.head
+        self.head = new_element
+
+    def delete_first(self):
+        if self.head:
+            deleted_element = self.head
+            temp = deleted_element.next
+            self.head = temp
+            return deleted_element
+        else:
             return None
-        while current and counter <= position:
-            if counter == position:
-                return current
-            current = current.next
-            counter += 1
-        return None
 
-    def insert(self, new_element, position):
-        counter = 1
-        current = self.head
-        if position > 1:
-            while current and counter < position:
-                if counter == position - 1:
-                    new_element.next = current.next
-                    current.next = new_element
-                current = current.next
-                counter += 1
-        elif position == 1:
-            new_element.next = self.head
-            self.head = new_element
+class Stack(object):
+    def __init__(self,top=None):
+        self.ll = LinkedList(top)
 
-    def delete(self, value):
-        current = self.head
-        previous = None
-        while current.value != value and current.next:
-            previous = current
-            current = current.next
-        if current.value == value:
-            if previous:
-                previous.next = current.next
-            else:
-                self.head = current.next
+    def push(self, new_element):
+        self.ll.insert_first(new_element)
+
+    def pop(self):
+        return self.ll.delete_first()
